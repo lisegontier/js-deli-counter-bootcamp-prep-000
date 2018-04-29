40003dd45615ce0169b5a7fb3982c44d9fc83c48
Liz Gunter <midwestunicorn@gmail.com>
2018-04-29 00:38:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,3 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
 }
 
-function nowServing(katzDeliLine, name) {
-  if {
-    
-  } else {
-    return "There is nodoby waiting to be served!";
-  }
-}
-
-function currentLine
