e24415f75570005b7a84f48da3c57fc1b36545ac
Liz Gunter <midwestunicorn@gmail.com>
2018-04-29 01:06:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ function currentLine(katzDeliLine) {
   for (let i = 0, l = katzDeliLine.length; i < l; i++) {
     numbersAndName.push(`${i + 1}. ${katzDeliLine[i]}`);
   }
-    return `The line is currently: ${numbersAndName.join(',')}`;
+    return `The line is currently: ${numbersAndName.join(', ')}`;
 }
