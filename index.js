265f47b9caa878a3ef8627ce7588293430d7e202
Liz Gunter <midwestunicorn@gmail.com>
2018-04-29 00:37:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+}
+
+function nowServing(katzDeliLine, name) {
+  if {
+    
+  } else {
+    return "There is nodoby waiting to be served!";
+  }
+}
+
+function currentLine
