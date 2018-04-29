e2900d1cc11b207d128268590890f938eda2aae5
Liz Gunter <midwestunicorn@gmail.com>
2018-04-29 00:45:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return 'Welcome ${name}, you are ${katzDeliLine.length}
+  return `Welcome ${name}, you are ${katzDeliLine.length} in line`;
 }
 
